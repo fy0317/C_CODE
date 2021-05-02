b8cbb3e7b007ae1b82e23def3b03f1129a1e65ae
fy0317 <81348431+fy0317@users.noreply.github.com>
2021-05-02 13:41:18 +0800

    Update game.h

--- 扫雷优化版/扫雷/game.h
+++ 扫雷优化版/扫雷/game.h
@@ -3,12 +3,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define ROW 9		//ÓĞĞ§ÆåÅÌĞĞÊı
-#define COL 9		//ÓĞĞ§ÆåÅÌÁĞÊı
-#define ROWS ROW+4	//Êµ¼ÊÆåÅÌĞĞÊı
-#define COLS COL+4	//Êµ¼ÊÆåÅÌÁĞÊı	   
-#define COUNT 1	//µØÀ×µÄÊıÄ¿
-void InitBoard(char board[ROWS][COLS], int row, int col, char c);	//³õÊ¼»¯ÆåÅÌ
-void PrintBoard(char board[ROWS][COLS], int row, int col);			//´òÓ¡ÆåÅÌ 
-void ArrangeBoard(char board[ROWS][COLS], int row, int col);		//²¼À×
-void FindBoard(char board[ROWS][COLS], char ShowBoard[ROWS][COLS],int													 row, int col);			//ÅÅÀ×
+#define ROW 9		//æœ‰æ•ˆæ£‹ç›˜è¡Œæ•°
+#define COL 9		//æœ‰æ•ˆæ£‹ç›˜åˆ—æ•°
+#define ROWS ROW+2	//å®é™…æ£‹ç›˜è¡Œæ•°
+#define COLS COL+2	//å®é™…æ£‹ç›˜åˆ—æ•°	   
+#define COUNT 1	//åœ°é›·çš„æ•°ç›®
+void InitBoard(char board[ROWS][COLS], int row, int col, char c);	//åˆå§‹åŒ–æ£‹ç›˜
+void PrintBoard(char board[ROWS][COLS], int row, int col);			//æ‰“å°æ£‹ç›˜ 
+void ArrangeBoard(char board[ROWS][COLS], int row, int col);		//å¸ƒé›·
+void FindBoard(char board[ROWS][COLS], char ShowBoard[ROWS][COLS],int													 row, int col);			//æ’é›·
